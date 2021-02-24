b4295fc1db30bacaabde3bd8ad918e4b75d95d6b
aiskanda <alexandre.iskandar@epfl.ch>
2021-02-24 14:27:21 +0100

    Finished 3.4

--- src/ch/epfl/tchu/game/Card.java
+++ src/ch/epfl/tchu/game/Card.java
@@ -4,15 +4,21 @@ import java.util.List;
 
 public enum Card {
 
-    BLACK,
-    VIOLET,
-    BLUE,
-    GREEN,
-    YELLOW,
-    ORANGE,
-    RED,
-    WHITE,
-    LOCOMOTIVE;
+    BLACK(Color.BLACK),
+    VIOLET(Color.VIOLET),
+    BLUE(Color.BLUE),
+    GREEN(Color.GREEN),
+    YELLOW(Color.YELLOW),
+    ORANGE(Color.ORANGE),
+    RED(Color.RED),
+    WHITE(Color.WHITE),
+    LOCOMOTIVE(null);
+
+    Card(Color color){
+        this.color = color;
+    }
+
+    private final Color color;
 
     public static final Card[] AllArray = Card.values();
 
@@ -21,5 +27,18 @@ public enum Card {
     public static final int COUNT = ALL.size();
 
     public static final List<Card> CARS = List.of(BLACK, VIOLET, BLUE,GREEN, YELLOW, ORANGE, RED, WHITE);
+
+    public static Card of(Color color){
+
+        for(int i = 0; i < COUNT; ++i){
+           if (color == CARS.get(i).color)
+               return CARS.get(i);
+        }
+        return null;
+    }
+
+    public Color color(){
+        return this.color;
+    }
 
 }
