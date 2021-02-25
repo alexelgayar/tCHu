9c1803c05097e4d1b72e10cb7532aece15aa32a2
aiskanda <alexandre.iskandar@epfl.ch>
2021-02-25 19:32:33 +0100

    Finished 3.9

--- src/ch/epfl/tchu/game/Ticket.java
+++ src/ch/epfl/tchu/game/Ticket.java
@@ -2,19 +2,28 @@ package ch.epfl.tchu.game;
 
 import ch.epfl.tchu.Preconditions;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
 
 public final class Ticket implements Comparable<Ticket> {
 
     private final String TEXT;
 
+   private List<Trip> trips;
+
     Ticket(List<Trip> trips) {
+
+        this.trips = trips;
 
         Preconditions.checkArgument(trips != null);
 
         for (int i = 0; i < trips.size() - 1; ++i) {
             Preconditions.checkArgument(trips.get(i).from() == trips.get(i + 1).from());
         }
+
+        TEXT = computeText();
+
     }
 
     Ticket(Station from, Station to, int points) {
@@ -22,19 +31,85 @@ public final class Ticket implements Comparable<Ticket> {
         this(List.of(new Trip(from, to, points)));
     }
 
-    private static String computeText(){
+    private String computeText(){
 
+        TreeSet<String> to = new TreeSet<>();
 
+        for (Trip w: trips) {
+            to.add(w.to().toString() + " (" + w.points() + ")");
+        }
+
+        String destinations = String.join(", ", to);
+
+        String s = new String();
+
+        if(trips.size() == 1){
+           s = String.format("%s - %s", trips.get(0).from().toString(), destinations);
+        }
+        else
+            s = String.format("%s - {%s}", trips.get(0).from().toString(), destinations);
+
+        return s;
     }
 
     public String text(){
         return TEXT;
     }
 
-    @Override
-    public int compareTo(Ticket o) {
-        return 0;
+    public int points(StationConnectivity connectivity){
+
+        int i = 0;
+        int temp = 0;
+
+        for(Trip w: trips) {
+          if(connectivity.connected(w.from(), w.to()) == true){
+             if (w.points() > temp)
+                 temp = w.points();
+          }
+          i = temp;
+        }
+
+        if (temp == 0){
+           temp = trips.get(0).points();
+            for(Trip w: trips) {
+                if(w.points() < temp){
+                    temp = w.points();
+                }
+            }
+            i = temp;
+        }
+        return i;
     }
+
+    @Override
+    public int compareTo(Ticket that) {
+
+        return this.compareTo(that);
+    }
+
+
+
+/*
+    public static void main(String[] args) {
+
+        Station Lausanne = new Station(0, "Lausanne");
+        Station Bern = new Station(1, "Bern");
+        Station Zurich = new Station(2, "Zurich");
+
+        List<Trip> trip = new ArrayList<>();
+        trip.add(new Trip(Lausanne, Bern, 9));
+        trip.add(new Trip(Lausanne, Zurich, 20));
+
+
+
+        Ticket ticket = new Ticket(trip);
+
+        System.out.println(ticket.text());
+
+
+    }
+
+ */
 
 
 }
