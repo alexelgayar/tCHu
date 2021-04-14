c0ed361d26ed8fb4be01a04ce161ce444821924a
Anirudhh Ramesh <anirudhh.ramesh@gmail.com>
2021-04-14 10:06:56 +0200

    - Shortened connected method of Station Partition

--- src/ch/epfl/tchu/game/StationPartition.java
+++ src/ch/epfl/tchu/game/StationPartition.java
@@ -19,26 +19,15 @@ public final class StationPartition implements StationConnectivity {
 
     /**
      * Returns true if both stations are connected, and if at least one of the stations is outside the bounds of the array, returns true iff both stations have the same id.
-     *
      * @param s1 First station
      * @param s2 Second station
      * @return true if both stations are connected, and if at least one of the stations is outside the bounds of the array, returns true iff both stations have the same id
      */
     @Override
     public boolean connected(Station s1, Station s2) {
-
-        if (s1.id() >= stationRepresentant.length || s2.id() >= stationRepresentant.length) {
-
-            if (s1.id() == s2.id())
-                return true;
-            else return false;
-
-        } else {
-
-            if (stationRepresentant[s1.id()] == stationRepresentant[s2.id()])
-                return true;
-            else return false;
-        }
+        return (s1.id() >= stationRepresentant.length || s2.id() >= stationRepresentant.length)
+                ? s1.id() == s2.id()
+                : stationRepresentant[s1.id()] == stationRepresentant[s2.id()];
     }
 
     /**
@@ -48,19 +37,14 @@ public final class StationPartition implements StationConnectivity {
     public static final class Builder {
 
         private int[] stationRepresentant;
-        private int stationCount;
 
         /**
          * Constructs a new builder and creates a list where every station is representative of itself
-         *
          * @param stationCount the number of stations contained in the list
          * @throws IllegalArgumentException if stationCount is strictly inferior than 0
          */
         public Builder(int stationCount) {
-
             Preconditions.checkArgument(stationCount >= 0);
-
-            this.stationCount = stationCount;
 
             stationRepresentant = new int[stationCount];
 
@@ -72,7 +56,6 @@ public final class StationPartition implements StationConnectivity {
 
         /**
          * Connects 2 stations by assigning the first station's representative to the second
-         *
          * @param s1 First station
          * @param s2 Second Station
          * @return a state identical to the receptor but with both stations connected
@@ -86,27 +69,21 @@ public final class StationPartition implements StationConnectivity {
 
         /**
          * Flattens the list then returns a stationPartition created with the new list
-         *
          * @return a StationPartition with a flattened list of representatives
          */
         public StationPartition build() {
-
             for (int i = 0; i < stationRepresentant.length; ++i) {
                 stationRepresentant[i] = representative(stationRepresentant[i]);
             }
-
             return new StationPartition(stationRepresentant);
         }
 
 
         private int representative(int id) {
-
             int a = id;
-
             while (stationRepresentant[a] != stationRepresentant[stationRepresentant[a]]) {
                 a = stationRepresentant[a];
             }
-
             return stationRepresentant[a];
         }
 
