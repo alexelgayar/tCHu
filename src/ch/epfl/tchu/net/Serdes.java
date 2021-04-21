c1721db8d0e2e593e2474b509c09cc25c4801eb9
aiskanda <alexandre.iskandar@epfl.ch>
2021-04-21 12:49:01 +0200

    Finished etape 7

--- src/ch/epfl/tchu/net/Serdes.java
+++ src/ch/epfl/tchu/net/Serdes.java
@@ -156,29 +156,5 @@ public class Serdes {
 
         }
     };
-
-
-    public static void main(String[] args) {
-
-        List<Card> fu = List.of(RED, WHITE, BLUE, BLACK, RED);
-        PublicCardState cs = new PublicCardState(fu, 30, 31);
-        List<Route> rs1 = ChMap.routes().subList(0, 2);
-        Map<PlayerId, PublicPlayerState> ps = Map.of(
-                PLAYER_1, new PublicPlayerState(10, 11, rs1),
-                PLAYER_2, new PublicPlayerState(20, 21, List.of()));
-        PublicGameState gs =
-                new PublicGameState(40, cs, PLAYER_2, ps, null);
-
-
-        System.out.println(publicGameStateSerde.serialize(gs).equals("40:6,7,2,0,6;30;31:1:10;11;0,1:20;21;:"));
-
-       String s = stringSerde.serialize("Charles");
-
-        System.out.println(stringSerde.deserialize(s));
-
-
-
-
-    }
 }
 
