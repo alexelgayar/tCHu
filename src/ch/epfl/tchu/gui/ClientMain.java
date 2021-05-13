198832fc7bed60a30caf8d6edeb61ab4d958a645
Anirudhh Ramesh <anirudhh.ramesh@gmail.com>
2021-05-13 17:47:50 +0200

    Added in GraphicalPlayerAdapter and ClientMain classes

---
+++ src/ch/epfl/tchu/gui/ClientMain.java
@@ -0,0 +1,10 @@
+package ch.epfl.tchu.gui;
+
+/**
+ * @author Alexandre Iskandar (324406)
+ * @author Anirudhh Ramesh (329806)
+ * Contains the main program of the tCHU client
+ */
+public class ClientMain {
+
+}
